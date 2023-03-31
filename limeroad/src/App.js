c62c6dc91f5c2a0a37b85c626949d4df08e623c7
Taniya Maurya <112754475+taniyamaurya@users.noreply.github.com>
2023-03-31 17:07:10 +0530

    day-3

--- limeroad/src/App.js
+++ limeroad/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './Components/Main';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Main/>
+      <Navbar/>
     </div>
   );
 }
